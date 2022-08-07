a8b4d4178a986b1ab3ffbac28681316b7fa12e15
Ro <83310470+DarkBlossom-One@users.noreply.github.com>
2022-08-07 11:24:03 -0700

    Delete Info.py

--- Info.py
+++
@@ -1,7 +0,0 @@
-TOKEN = 'ODYzNDg0MzkxMDc3NDQ1NjMy.GNfDQV.wd2q4fFJPHZ8Im2EhU6HKoSALuaAdmmsy3YiJY'
-
-FunFacts = [
-    'Yumi clips everything',
-    'King likes monkeys',
-    'Critical studiio game coming 2023 ',
-]
