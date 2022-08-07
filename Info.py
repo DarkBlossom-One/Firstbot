7773bdb0c1aa9ce0e37f7974e5c565075a8c90a7
Ro <83310470+DarkBlossom-One@users.noreply.github.com>
2022-08-07 11:23:47 -0700

    Add files via upload

---
+++ Info.py
@@ -0,0 +1,7 @@
+TOKEN = 'ODYzNDg0MzkxMDc3NDQ1NjMy.GNfDQV.wd2q4fFJPHZ8Im2EhU6HKoSALuaAdmmsy3YiJY'
+
+FunFacts = [
+    'Yumi clips everything',
+    'King likes monkeys',
+    'Critical studiio game coming 2023 ',
+]
